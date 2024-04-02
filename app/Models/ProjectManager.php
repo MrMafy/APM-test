e04e4f0bddf3b663c039a733fcc80fc78b8a9bb6
Katherine Samsonova <ktsmsnv@yandex.ru>
2024-04-02 15:22:27 +0500

    личный кабинет

--- app/Models/ProjectManager.php
+++ app/Models/ProjectManager.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectManager extends Model
 {
     use HasFactory;
-    protected $fillable = ['fio'];
+    protected $table = 'project_managers';
+    protected $fillable = ['fio', 'groupNum'];
 }
