4c28147b31637b58d50fc905a19c8eb8b601748e
Khalil <44137539+MrMafy@users.noreply.github.com>
2024-07-16 12:24:49 +0500

    Обновление ветки 16.07.2024 - 12:23
    Настроена переадресация с https на http временно.

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: process.env.APP_URL,
-    server: {
-        proxy: {
-          '/api': {
-            target: 'http://192.168.60.35:5173',
-            changeOrigin: true,
-            secure: false,
-          },
-        },
-      },
+    // base: process.env.APP_URL,
+    // server: {
+    //     proxy: {
+    //       '/api': {
+    //         target: 'https://192.168.60.35:5173',
+    //         changeOrigin: true,
+    //         secure: false,
+    //       },
+    //     },
+    //   },
     plugins: [
         laravel({
             input: [
@@ -30,6 +30,14 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        // https: {
+        //     key: fs.readFileSync(path.resolve(__dirname, 'storage/app/ssl/privat_key.key')),
+        //     cert: fs.readFileSync(path.resolve(__dirname, 'storage/app/ssl/certificate.crt')),
+        // },
+        host: '192.168.60.35',
+        port: 5173,
+    },
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
